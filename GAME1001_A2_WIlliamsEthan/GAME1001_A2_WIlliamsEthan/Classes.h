d1a3c411732291909a92b52a13bde22630fac5c9
DoorKnoob <ethanwilliams11@hotmail.ca>
2018-12-02 15:10:42 -0500

    Work

--- GAME1001_A2_WIlliamsEthan/GAME1001_A2_WIlliamsEthan/Classes.h
+++ GAME1001_A2_WIlliamsEthan/GAME1001_A2_WIlliamsEthan/Classes.h
@@ -7,17 +7,44 @@ public:
 	bool m_owned;
 	string m_name;
 	string m_sound; 
+	int m_cost; 
 
 	Weapon() //Default Constructor
 	{
 		
 	}
 	
-	Weapon(bool m_owned, string m_name, string m_sound) //Constructor
+	Weapon(bool m_owned, string m_name, string m_sound, int m_cost) //Constructor
 	{
-		m_owned; 
-		m_name;
-		m_sound;
+		this-> m_owned = m_owned; 
+		this-> m_name = m_name;
+		this->m_sound = m_sound;
+		this->m_cost = m_cost;
+	}
+
+	string getName()
+	{
+		return m_name; 
+	}
+
+	int getCost()
+	{
+		return m_cost; 
+	}
+
+	string getSound()
+	{
+		return m_sound; 
+	}
+
+	bool getOwned()
+	{
+		return m_owned;
+	}
+
+	void setOwned(bool o)
+	{
+		m_owned = o; 
 	}
 };
 
--- GAME1001_A2_WIlliamsEthan/GAME1001_A2_WIlliamsEthan/Main.cpp
+++ GAME1001_A2_WIlliamsEthan/GAME1001_A2_WIlliamsEthan/Main.cpp
@@ -1,91 +1,203 @@
 #include <iostream>
 #include <string>
 #include "Classes.h"
-#define NUM 5 // In C++11 we can also use constexpr, but it's different.
 using namespace std;
-
-void printMenu(void)
-{
-	cout << "\nBuy Weapon [B]\nCheck Balance [C]\nExit Store [X]\n\nEnter Option: ";
-}
 
 void printMoney(const int& m)
 {
 	cout << "Balance: $" << m << endl;
 }
 
-void printBuy(const string* n, const int* c, const bool* o)
-{
-	for (int i = 0; i < NUM; i++)
-	{
-		cout << "\n" << "[" << i + 1 << "] " << n[i];
-		cout << ": $" << c[i] << (o[i] == true ? " -Owned" : "");
-	}
-	cout << "\nReturn to Store [X]\n\nEnter Number to Buy: ";
-}
-
 int main()
 {
+	Weapon weapon[5] = { Weapon(false, "Pistol", "Bang Bang!!!", 100),
+	 Weapon(false, "Rifle", "Bratata!!", 200 ),
+	 Weapon(false, "Sword", "Shiiiiiiiiiinggg!", 300),
+	 Weapon(false, "Shotgun", "BOOM Chk Chk!!!", 500),
+	 Weapon(false, "Rocket Launcher", "Booooooom!!!", 750) };
 	
-	
-	Weapon weapon[5] = { Weapon(false, "Pistol", "Bang Bang!!!"),
-	 Weapon(false, "Rifle", "Bratata!!"),
-	 Weapon(false, "Sword", "Shiiiiiiiiiinggg!"),
-	 Weapon(false, "Shotgun", "BOOM Chk Chk!!!"),
-	 Weapon(false, "Pistol", "Bang Bang!!!") };
-	
-	bool wOwned[NUM] = { false };
-	string wNames[NUM] = { "Pistol", "Shotgun", "SMG", "Rifle", "Bazooka" };
-	string wActions[NUM] = { "BANG!", "BOOM!", "BRR-RAK-AK-AK-AK-AK-AK!", "RAT-TAT-TAT-TAT-TAT!", "FOOSSS-KABOOM!" };
-	int wCosts[5] = { 100, 250, 500, 750, 1000 };
 
-	bool buy, inStore = true;
-	char optM, optB;
-	int optI, numWeapons = 0, pMoney = 1000;
+	bool inStore = true, viewStore, inSimulation = true;
+	char option;
+	int numWeapons = 0, pMoney = 1000;
+	
 
-	while (inStore) // Store stage.
+
+
+	while (inStore == true) // Store stage.
 	{
-		printMenu();
-		cin >> optM;
-		if (optM == 'b' || optM == 'B')
+		viewStore = true;
+		cout << "Hello and welcome to the store. What would you like to do?\n Buy a Weapon (B), check your balance (C), or exit the store and proceed to the simulation? (X)";
+		cin >> option; 
+		
+		if (option == 'B' || option == 'b') // this checks for lower and uppercase
 		{
-			buy = true;
-			while (buy == true)
+			
+			while (viewStore == true)
 			{
-				printBuy(wNames, wCosts, wOwned);
-				cin >> optB;
-				if (optB == 'x' || optB == 'X')
-					buy = 0;
-				else
+				for (int i = 0; i < 5; i++)
 				{
-					optI = optB - 49;
-					if (wOwned[optI] == true) // Already has weapon
-						cout << "Weapon already owned!" << endl;
-					else if (wCosts[optI] > pMoney) // Can't afford weapon
-						cout << "You cannot afford that weapon!" << endl;
-					else
+					cout << weapon[i].getName() << "\t $" << weapon[i].getCost() << endl;
+				}
+
+				cout << "Please select the weapon you would like to buy or, press E if you would like to leave.\n";
+				cin >> option;
+
+
+
+				if (option == '1')
+				{
+					if (pMoney < weapon[0].getCost())
 					{
-						pMoney -= wCosts[optI];
-						wOwned[optI] = true;
-						numWeapons++;
-						cout << "Weapon purchased!" << endl;
+						cout << "Sorry you don't have enough for that weapon\n";
 					}
+					else if (pMoney >= weapon[0].getCost())
+					{
+						weapon[0].setOwned(true);
+						pMoney -= weapon[0].getCost();
+						cout << weapon[0].getName() << " purchased" << endl;
+					}
+				}
+
+				else if (option == '2')
+				{
+					if (pMoney < weapon[1].getCost())
+					{
+						cout << "Sorry you don't have enough for that weapon\n";
+					}
+					else if (pMoney >= weapon[1].getCost())
+					{
+						weapon[1].setOwned(true);
+						pMoney -= weapon[1].getCost();
+						cout << weapon[1].getName() << " purchased" << endl;
+					}
+				}
+
+				else if (option == '3')
+				{
+					if (pMoney < weapon[2].getCost())
+					{
+						cout << "Sorry you don't have enough for that weapon\n";
+					}
+					else if (pMoney >= weapon[2].getCost())
+					{
+						weapon[2].setOwned(true);
+						pMoney -= weapon[2].getCost();
+						cout << weapon[2].getName() << " purchased" << endl;
+					}
+				}
+
+				else if (option == '4')
+				{
+					if (pMoney < weapon[3].getCost())
+					{
+						cout << "Sorry you don't have enough for that weapon\n";
+					}
+					else if (pMoney >= weapon[3].getCost())
+					{
+						weapon[3].setOwned(true);
+						pMoney -= weapon[3].getCost();
+						cout << weapon[3].getName() << " purchased" << endl;
+					}
+				}
+
+				else if (option == '5')
+				{
+					if (pMoney < weapon[3].getCost())
+					{
+						cout << "Sorry you don't have enough for that weapon\n";
+					}
+					else if (pMoney >= weapon[4].getCost())
+					{
+						weapon[4].setOwned(true);
+						pMoney -= weapon[4].getCost();
+						cout << weapon[4].getName() << " purchased" << endl;
+					}
+				}
+
+				else if (option == 'E' || 'e')
+				{
+					viewStore = false; 
 				}
 			}
 		}
-		else if (optM == 'c' || optM == 'C')
+
+		if (option == 'C' || option == 'c')
 		{
-			printMoney(pMoney);
+			cout << "\nYour current balance is $" << pMoney << endl;
 		}
-		else if (optM == 'x' || optM == 'X')
+
+		if (option == 'X' || option == 'x')
 		{
-			if (numWeapons > 0)
-				inStore = false;
+
+			if (!weapon[0].getOwned() && !weapon[1].getOwned() && !weapon[2].getOwned() && !weapon[3].getOwned() && !weapon[4].getOwned())
+			{
+				cout << "You don't own any weapons yet!" << endl;
+			}
 			else
-				cout << "You cannot exit the store without buying one weapon!\n";
+			{
+				inStore = false;
+				cout << "Have fun!" << endl;
+			}
 		}
 	}
-	cout << "\nGood-bye!" << endl;
+
+	while (inSimulation == true)
+	{
+		//system("cls");
+		cout << "Welcome to the simulation. Please select the weapon that you would like to equip." << endl;
+
+		for (int i = 0; i < 5; i++)
+		{
+			if (weapon[i].getOwned())
+			{
+				cout << weapon[i].getName() << endl;
+			}
+		}
+		cin >> option; 
+
+		if (option == '1')
+		{
+			cout << "You have chosen the pistol. Press E to shoot.\n"; 
+
+		}
+
+
+		if (option == '2')
+		{
+			cout << "You have chosen the rifle. Press E to shoot.\n";
+		}
+
+		if (option == '3')
+		{
+			cout << "You have chosen the sword. Press E to shoot.\n";
+		}
+
+		if (option == '4')
+		{
+			cout << "You have chosen the shotgun. Press E to shoot.\n";
+		}
+
+		if (option == '5')
+		{
+			cout << "You have chosen the rocket launcher. Press E to shoot.\n";
+		}
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 	system("pause");
 	return 0;
 }
